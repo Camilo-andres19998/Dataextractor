1efeb9213db0bd7a20ba12ff394c1238e14c0074
Camilo-andres19998 <andrescamilopoblete@gmail.com>
2023-05-15 19:38:54 -0400

    Crear modelo Bikestation

--- Appdataextractor/models.py
+++ Appdataextractor/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class BikeStation(models.Model):
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    free_bikes = models.IntegerField()
+    empty_slots = models.IntegerField()
+
+    def __str__(self):
+        return self.name

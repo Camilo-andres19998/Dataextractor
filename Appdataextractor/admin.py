09183fbff30c460ba780fe5c85fc7831f5664d5c
Camilo-andres19998 <andrescamilopoblete@gmail.com>
2023-05-16 00:03:22 -0400

    Cambios en diseño tabla

--- Appdataextractor/admin.py
+++ Appdataextractor/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import BikeStation
 
+
+
 class BikeStationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'latitude', 'longitude', 'free_bikes', 'empty_slots')
-    list_filter = ('name',)  
-    search_fields = ('name',)  
+    list_display = ('id', 'name', 'latitude', 'longitude', 'free_bikes', 'empty_slots')
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+    ordering = ['id']  
 
 admin.site.register(BikeStation, BikeStationAdmin)

2325066bcb118e41bd60b0956c3ecda095199a82
Camilo-andres19998 <andrescamilopoblete@gmail.com>
2023-05-15 23:29:03 -0400

    Generar vista para administrador

--- Appdataextractor/admin.py
+++ Appdataextractor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BikeStation
 
-# Register your models here.
+class BikeStationAdmin(admin.ModelAdmin):
+    list_display = ('name', 'latitude', 'longitude', 'free_bikes', 'empty_slots')
+    list_filter = ('name',)  
+    search_fields = ('name',)  
+
+admin.site.register(BikeStation, BikeStationAdmin)
--- Appdataextractor/models.py
+++ Appdataextractor/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-from django.db import models
+
 
 class BikeStation(models.Model):
     name = models.CharField(max_length=100)
